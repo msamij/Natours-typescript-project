671dc542beedb0f848448baf8894f17237b76fa8
Muhammad Sami <msamiaj@outlook.com>
2025-10-09 05:13:15 +0500

    feat(tourController.ts): added aggregation pipeline feature controller

--- src/routes/tourRoutes.ts
+++ src/routes/tourRoutes.ts
@@ -5,6 +5,8 @@ const router: Router = express.Router();
 
 router.route('/top-5-cheap').get(tourController.aliasTopTour, tourController.getAllTours);
 
+router.route('/tour-stats').get(tourController.getTourStats);
+
 router.route('/').get(tourController.getAllTours).post(tourController.createTour);
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
 
