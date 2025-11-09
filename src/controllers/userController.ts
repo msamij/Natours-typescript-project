927d4ff423840efd81833d33cb93cddbfedc921d
Muhammad Sami <msamiaj@outlook.com>
2025-11-09 09:50:32 +0500

    feat(userController.ts): finished controller to update user data

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,6 +2,19 @@ import { type NextFunction, type Request, type Response } from 'express';
 import User from '../models/userModel.js';
 import { catchAsync } from '../utils/catchAsync.js';
 import { AppError } from '../utils/appError.js';
+import type { RequestWithUser } from '../types/Types.js';
+import { logger } from '../logger.js';
+
+const filterObj = (obj: any, ...allowedFields: string[]) => {
+  const newObj: any = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) {
+      newObj[el] = obj[el];
+    }
+  });
+
+  return newObj;
+};
 
 export const getAllUsers = catchAsync(async (_req: Request, res: Response) => {
   const users = await User.find();
@@ -15,13 +28,20 @@ export const getAllUsers = catchAsync(async (_req: Request, res: Response) => {
   });
 });
 
-export const updateMe = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+export const updateMe = catchAsync(async (req: RequestWithUser, res: Response, next: NextFunction) => {
   if (req.body.password || req.body.passwordConfirm) {
     return next(new AppError('This route is not for password updates. Please use /updateMyPassword', 400));
   }
 
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, { new: true, runValidators: true });
+
   res.status(200).json({
     status: 'success',
+    data: {
+      user: updatedUser,
+    },
   });
 });
 
