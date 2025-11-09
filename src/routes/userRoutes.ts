868e6ffe34a9a0f74e98cc498a925b756518b83e
Muhammad Sami <msamiaj@outlook.com>
2025-11-09 10:48:56 +0500

    (userRoutes.ts): added /deleteMe route

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -12,6 +12,7 @@ router.post('/forgotPassword', authController.forgotPassword);
 
 router.patch('/updateMyPassword', authController.protect, authController.updatePassword);
 router.patch('/updateMe', authController.protect, userController.updateMe);
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
 router.route('/').get(userController.getAllUsers).post(userController.createUser);
 router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
