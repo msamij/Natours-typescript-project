fb136f6772a06ef79d847ad51af91f49441e55ea
Muhammad Sami <msamiaj@outlook.com>
2025-11-19 03:34:01 +0500

    feat(tourModel.ts): added pre save middleware to embed userDocs in tourDocs

--- src/models/tourModel.ts
+++ src/models/tourModel.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import slugify from '../imports/slugify.js';
 import { logger } from '../logger.js';
+import User from './userModel.js';
 
 const tourSchema = new mongoose.Schema(
   {
@@ -100,6 +101,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -119,6 +121,12 @@ type TourQueryContext = mongoose.Query<any, any> & TourSchemaInferred;
 
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
+});
+
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async id => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
+  next();
 });
 
 tourSchema.pre('save', function (next) {
