a964d9c978c9dbee82606b039ac0ead81a0556ba
Muhammad Sami <msamiaj@outlook.com>
2026-01-22 05:39:05 +0500

    added mergeParams to true in expressRouter

--- src/routes/reviewRoutes.ts
+++ src/routes/reviewRoutes.ts
@@ -2,7 +2,9 @@ import express, { Router } from 'express';
 import { protect, restrictTo } from '../controllers/authController.js';
 import * as reviewController from '../controllers/reviewController.js';
 
-const router: Router = express.Router();
+const router: Router = express.Router({
+  mergeParams: true,
+});
 
 router.route('/').get(reviewController.getAllReviews).post(protect, restrictTo('user'), reviewController.createReview);
 
