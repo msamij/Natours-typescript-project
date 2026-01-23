36f52cf2520f4b14278431159ac9889e56ebcf6e
Muhammad Sami <msamiaj@outlook.com>
2026-01-23 06:52:28 +0500

    added /:id route for deleteReview

--- src/routes/reviewRoutes.ts
+++ src/routes/reviewRoutes.ts
@@ -7,5 +7,6 @@ const router: Router = express.Router({
 });
 
 router.route('/').get(reviewController.getAllReviews).post(protect, restrictTo('user'), reviewController.createReview);
+router.route('/:id').delete(reviewController.deleteReview);
 
 export default router;
