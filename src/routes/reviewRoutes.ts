d50d8fd4194b8af0d5197ba1748f13bf8f195b30
Muhammad Sami <msamiaj@outlook.com>
2026-01-25 10:48:20 +0500

    feat(reviewRoutes.ts): add setTourUserIds middleware under / route

--- src/routes/reviewRoutes.ts
+++ src/routes/reviewRoutes.ts
@@ -6,7 +6,11 @@ const router: Router = express.Router({
   mergeParams: true,
 });
 
-router.route('/').get(reviewController.getAllReviews).post(protect, restrictTo('user'), reviewController.createReview);
+router
+  .route('/')
+  .get(reviewController.getAllReviews)
+  .post(protect, restrictTo('user'), reviewController.setTourUserIds, reviewController.createReview);
+
 router.route('/:id').patch(reviewController.updateReview).delete(reviewController.deleteReview);
 
 export default router;
