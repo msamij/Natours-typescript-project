cc7da0f17cc3652e68211501f082d67cbf777db5
Muhammad Sami <msamiaj@outlook.com>
2026-01-28 04:46:38 +0500

    refactor(reviewController.ts): add getAllReviews factory

--- src/controllers/reviewController.ts
+++ src/controllers/reviewController.ts
@@ -1,25 +1,7 @@
 import type { NextFunction, Request, Response } from 'express';
 import Review from '../models/reviewModel.js';
 import type { RequestWithUser } from '../types/Types.js';
-import { catchAsync } from '../utils/catchAsync.js';
 import * as factory from './handlerFactory.js';
-
-export const getAllReviews = catchAsync(async (req: Request, res: Response, _next: NextFunction) => {
-  let filter = {};
-  if (req.params.tourId) {
-    filter = { tour: req.params.tourId };
-  }
-
-  const reviews = await Review.find(filter);
-
-  res.status(200).json({
-    status: 'success',
-    results: reviews.length,
-    data: {
-      reviews,
-    },
-  });
-});
 
 export const setTourUserIds = (req: Request, _res: Response, next: NextFunction) => {
   const request = req as RequestWithUser;
@@ -28,6 +10,8 @@ export const setTourUserIds = (req: Request, _res: Response, next: NextFunction)
   if (!request.body.user) request.body.user = request.user.id;
   next();
 };
+
+export const getAllReviews = factory.getAll(Review);
 
 export const createReview = factory.createOne(Review);
 
