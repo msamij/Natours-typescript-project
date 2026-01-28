b0b4eb80c54f05b9e0acfbf06473d2410b1a01c6
Muhammad Sami <msamiaj@outlook.com>
2026-01-28 05:36:12 +0500

    refactor(reviewRoutes.ts): add restrictTo middlware for udpate and delete review

--- src/routes/reviewRoutes.ts
+++ src/routes/reviewRoutes.ts
@@ -16,7 +16,7 @@ router
 router
   .route('/:id')
   .get(reviewController.getReview)
-  .patch(reviewController.updateReview)
-  .delete(reviewController.deleteReview);
+  .patch(restrictTo('user', 'admin'), reviewController.updateReview)
+  .delete(restrictTo('user', 'admin'), reviewController.deleteReview);
 
 export default router;
