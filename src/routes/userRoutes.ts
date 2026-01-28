3fe52eaa80f3b5d60b6098f47b44e806e6f9468b
Muhammad Sami <msamiaj@outlook.com>
2026-01-28 05:26:56 +0500

    refactor(userRoutes.ts): add restrictTo middlware for getAll and getOne users

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -6,17 +6,17 @@ const router: Router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-router.patch('/resetPassword/:token', authController.resetPassword);
 router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
 
 router.use(authController.protect);
 
 router.patch('/updateMyPassword', authController.updatePassword);
-
 router.get('/me', userController.getMe, userController.getUser);
-
 router.patch('/updateMe', userController.updateMe);
 router.delete('/deleteMe', userController.deleteMe);
+
+router.use(authController.restrictTo('admin'));
 
 router.route('/').get(userController.getAllUsers).post(userController.createUser);
 router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
