0a0803ea2655fb43ecdc8652711f75dea05924d3
Muhammad Sami <msamiaj@outlook.com>
2026-01-28 05:09:30 +0500

    refactor(tourRoutes.ts): remove protect middleware for getAllTours

--- src/routes/tourRoutes.ts
+++ src/routes/tourRoutes.ts
@@ -12,7 +12,7 @@ router.route('/top-5-cheap').get(tourController.aliasTopTour, tourController.get
 router.route('/tour-stats').get(tourController.getTourStats);
 router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
-router.route('/').get(authController.protect, tourController.getAllTours).post(tourController.createTour);
+router.route('/').get(tourController.getAllTours).post(tourController.createTour);
 
 router
   .route('/:id')
