39e7a03c4b049a698bce63e8305686e3225ae48e
Muhammad Sami <msamiaj@outlook.com>
2026-01-28 05:18:41 +0500

    refactor(tourRoutes.ts): add protect middleware for all user update controllers

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -6,14 +6,17 @@ const router: Router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-
 router.patch('/resetPassword/:token', authController.resetPassword);
 router.post('/forgotPassword', authController.forgotPassword);
-router.patch('/updateMyPassword', authController.protect, authController.updatePassword);
 
-router.get('/me', authController.protect, userController.getMe, userController.getUser);
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+router.use(authController.protect);
+
+router.patch('/updateMyPassword', authController.updatePassword);
+
+router.get('/me', userController.getMe, userController.getUser);
+
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
 
 router.route('/').get(userController.getAllUsers).post(userController.createUser);
 router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
