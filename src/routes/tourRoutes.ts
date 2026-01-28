f8f0b2b18a52bde0cb65dddc5363e629953d77f1
Muhammad Sami <msamiaj@outlook.com>
2026-01-28 05:10:43 +0500

    refactor(tourRoutes.ts): add protect and restrict middleware for createTour

--- src/routes/tourRoutes.ts
+++ src/routes/tourRoutes.ts
@@ -12,7 +12,10 @@ router.route('/top-5-cheap').get(tourController.aliasTopTour, tourController.get
 router.route('/tour-stats').get(tourController.getTourStats);
 router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
-router.route('/').get(tourController.getAllTours).post(tourController.createTour);
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.createTour);
 
 router
   .route('/:id')
