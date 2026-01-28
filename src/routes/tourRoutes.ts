50e7662b8061971d8955630eea1a3b724dfc558d
Muhammad Sami <msamiaj@outlook.com>
2026-01-28 05:13:56 +0500

    refactor(tourRoutes.ts): add protect and restrict middleware for getMonthlyPlan

--- src/routes/tourRoutes.ts
+++ src/routes/tourRoutes.ts
@@ -10,7 +10,9 @@ router.use('/:tourId/reviews', reviewRouter);
 router.route('/top-5-cheap').get(tourController.aliasTopTour, tourController.getAllTours);
 
 router.route('/tour-stats').get(tourController.getTourStats);
-router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+router
+  .route('/monthly-plan/:year')
+  .get(authController.protect, authController.restrictTo('admin', 'lead-guide', 'guide'), tourController.getMonthlyPlan);
 
 router
   .route('/')
