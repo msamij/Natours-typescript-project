ad3c7c344480c0835bd52ec2415a39b5a807218b
Muhammad Sami <msamiaj@outlook.com>
2026-02-24 11:03:24 +0500

    feat: add getTour views Controller

--- src/controllers/viewsController.ts
+++ src/controllers/viewsController.ts
@@ -1,13 +1,24 @@
 import Tour from '../models/tourModel.js';
 import type { NextFunction, Request, Response } from 'express';
 import { catchAsync } from '../utils/catchAsync.js';
+import type { RequestWithSlug } from '../types/Types.js';
 
 export const getOverview = catchAsync(async (_req: Request, res: Response, _next: NextFunction) => {
   const tours = await Tour.find();
-  res.status(200).render('overview', { title: 'All Tours', tours });
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours,
+  });
 });
 
-export const getTour = (_req: Request, res: Response) => {
-  const tour = await Tour.findOne();
-  res.status(200).render('tour', { title: 'The Forest Hiker' });
-};
+export const getTour = catchAsync(async (req: RequestWithSlug, res: Response) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    select: 'review rating user',
+  });
+
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker',
+    tour,
+  });
+});
