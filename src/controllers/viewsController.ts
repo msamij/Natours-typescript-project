d2b3fb1e971c3f4de1dba9fab5f756dae4a84558
Muhammad Sami <msamiaj@outlook.com>
2026-02-24 10:43:22 +0500

    feat: finished overview page render

--- src/controllers/viewsController.ts
+++ src/controllers/viewsController.ts
@@ -1,8 +1,11 @@
-import type { Request, Response } from 'express';
+import Tour from '../models/tourModel.js';
+import type { NextFunction, Request, Response } from 'express';
+import { catchAsync } from '../utils/catchAsync.js';
 
-export const getOverview = (_req: Request, res: Response) => {
-  res.status(200).render('overview', { title: 'All Tours' });
-};
+export const getOverview = catchAsync(async (_req: Request, res: Response, _next: NextFunction) => {
+  const tours = await Tour.find();
+  res.status(200).render('overview', { title: 'All Tours', tours });
+});
 
 export const getTour = (_req: Request, res: Response) => {
   res.status(200).render('tour', { title: 'The Forest Hiker' });
