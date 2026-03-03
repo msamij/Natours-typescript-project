b9ddbdf7b0f0d7bf936dd7d8fbef87afad788849
Muhammad Sami <msamiaj@outlook.com>
2026-03-03 11:12:16 +0500

    refactor: export login function in index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,9 @@
-console.log('Hello from esbuild!');
+import { login } from './login';
+
+document.querySelector('.form').addEventListener('submit', e => {
+  e.preventDefault();
+
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  login(email, password);
+});
