427f9d4928b86706612e5823b1a3d506c9894dce
Muhammad Sami <msamiaj@outlook.com>
2026-03-04 17:52:23 +0500

    feat(viewController.ts): add guard clause for getTour in case no tour was found

--- src/controllers/viewsController.ts
+++ src/controllers/viewsController.ts
@@ -2,6 +2,7 @@ import type { NextFunction, Request, Response } from 'express';
 import Tour from '../models/tourModel.js';
 import type { RequestWithSlug } from '../types/Types.js';
 import { catchAsync } from '../utils/catchAsync.js';
+import { AppError } from '../utils/appError.js';
 
 export const getOverview = catchAsync(async (_req: Request, res: Response, _next: NextFunction) => {
   const tours = await Tour.find();
@@ -11,11 +12,15 @@ export const getOverview = catchAsync(async (_req: Request, res: Response, _next
   });
 });
 
-export const getTour = catchAsync(async (req: RequestWithSlug, res: Response, _next: NextFunction) => {
+export const getTour = catchAsync(async (req: RequestWithSlug, res: Response, next: NextFunction) => {
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
     select: 'review rating user',
   });
+
+  if (!tour) {
+    return next(new AppError('There is no tour with that name.', 404));
+  }
 
   res.status(200).render('tour', {
     title: `${tour?.name} Tour`,
