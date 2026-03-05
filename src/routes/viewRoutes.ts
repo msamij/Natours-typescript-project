3dbc977bf84f7f9577de12d0de39095c38ca920f
Muhammad Sami <msamiaj@outlook.com>
2026-03-05 15:44:04 +0500

    refactor: removed isLoggedIn from all routes added isLoggedIn only for overview, tour and loginform page.

--- src/routes/viewRoutes.ts
+++ src/routes/viewRoutes.ts
@@ -1,14 +1,12 @@
 import express, { Router } from 'express';
-import * as viewController from '../controllers/viewsController.js';
 import { isLoggedIn, protect } from '../controllers/authController.js';
+import * as viewController from '../controllers/viewsController.js';
 
 const router: Router = express.Router();
 
-router.use(isLoggedIn);
-
-router.get('/', viewController.getOverview);
-router.get('/tour/:slug', viewController.getTour);
-router.get('/login', viewController.getLoginForm);
+router.get('/', isLoggedIn, viewController.getOverview);
+router.get('/tour/:slug', isLoggedIn, viewController.getTour);
+router.get('/login', isLoggedIn, viewController.getLoginForm);
 router.get('/me', protect, viewController.getAccount);
 
 export default router;
