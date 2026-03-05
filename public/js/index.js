0ad0efaf2160ee1e27683b1de3414a323a618691
Muhammad Sami <msamiaj@outlook.com>
2026-03-05 22:15:32 +0500

    feat: update user data on frontend using API

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,9 @@
 import { login, logout } from './login';
+import { updateData } from './updateSettings';
 
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
 
 if (loginForm) {
   loginForm.addEventListener('submit', e => {
@@ -15,4 +17,13 @@ if (loginForm) {
 
 if (logOutBtn) {
   logOutBtn.addEventListener('click', logout);
+}
+
+if (userDataForm) {
+  userDataForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateData(name, email);
+  });
 }
