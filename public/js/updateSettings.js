80474e8c4884aee85b12500504f1843180342b65
Muhammad Sami <msamiaj@outlook.com>
2026-03-06 19:36:12 +0500

    feat: update passwords on the front-end

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -1,19 +1,18 @@
 import axios from 'axios';
 import { showAlert } from './alert';
 
-export const updateData = async (name, email) => {
+export const updateSettings = async (data, type) => {
   try {
+    const urlEndPoint = type === 'password' ? 'updateMyPassword' : 'updateMe';
+
     const res = await axios({
       method: 'PATCH',
-      url: '/api/v1/users/updateMe',
-      data: {
-        name,
-        email,
-      },
+      url: `/api/v1/users/${urlEndPoint}`,
+      data,
     });
 
     if (res.data.status === 'success') {
-      showAlert('success', 'Data updated successfully!');
+      showAlert('success', `${type.toUpperCase()} updated successfully!`);
     }
   } catch (err) {
     showAlert('error', err.response.data.message);
