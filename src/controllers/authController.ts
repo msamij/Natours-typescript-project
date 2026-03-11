4d9758dc57ac765152bd9435fb4ff64305d9d7df
Muhammad Sami <msamiaj@outlook.com>
2026-03-11 11:47:27 +0500

    feat: add send welcome email on signup

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,10 +3,10 @@ import { type NextFunction, type Request, type RequestHandler, type Response } f
 import jwt from 'jsonwebtoken';
 import { Types } from 'mongoose';
 import User from '../models/userModel.js';
+import { Email } from '../utils/email.js';
 import type { RequestWithToken, RequestWithUser } from '../types/Types.js';
 import { AppError } from '../utils/appError.js';
 import { catchAsync } from '../utils/catchAsync.js';
-import { sendEmail } from '../utils/email.js';
 import { jwtVerifyPromisified } from '../utils/jwtVerifyPromisify.js';
 
 const signToken = (id: Types.ObjectId) => {
@@ -50,6 +50,9 @@ export const signup = catchAsync(async (req: Request, res: Response, _next: Next
     passwordConfirm: req.body.passwordConfirm,
     role: req.body?.role,
   });
+
+  const url = `${req.protocol}://${req.get('host')}/me`;
+  await new Email(newUser, url).sendWelcome();
 
   createSendToken(newUser, 201, res);
 });
@@ -158,11 +161,11 @@ export const forgotPassword = catchAsync(async (req: Request, res: Response, nex
   ${resetURL}\nIf you didn't forget your password, please ignore this email!`;
 
   try {
-    await sendEmail({
-      email: user.email,
-      subject: 'Your password reset token (valid for 10 min)',
-      message,
-    });
+    // await sendEmail({
+    //   email: user.email,
+    //   subject: 'Your password reset token (valid for 10 min)',
+    //   message,
+    // });
 
     res.status(200).json({
       status: 'success',
--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,4 +1,4 @@
-import htmlToText from 'html-to-text';
+import { htmlToText } from 'html-to-text';
 import nodemailer from 'nodemailer';
 import pug from 'pug';
 import type { UserModelProps } from '../models/userModel.js';
@@ -16,29 +16,7 @@ export class Email {
     this.from = `Muhammad Sami <${process.env.EMAIL_FROM}>`;
   }
 
-  async send(template: string, subject: string) {
-    const html = pug.renderFile(`${import.meta.dirname}/../views/email/${template}.pug`, {
-      firstName: this.firstName,
-      url: this.url,
-      subject,
-    });
-
-    const mailOptions = {
-      from: this.from,
-      to: this.to,
-      subject,
-      html,
-      text: htmlToText.htmlToText(html),
-    };
-
-    await this.createTransport()?.sendMail(mailOptions);
-  }
-
-  async sendWelcome() {
-    await this.send('welcome', 'Welcome to the Natours Family!');
-  }
-
-  private createTransport() {
+  private newTransport() {
     if (process.env.NODE_ENV === 'production') {
       // Sendgrid
       return;
@@ -51,5 +29,27 @@ export class Email {
         pass: process.env.EMAIL_PASSWORD,
       },
     });
+  }
+
+  private async send(template: string, subject: string) {
+    const html = pug.renderFile(`${import.meta.dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject,
+    });
+
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText(html),
+    };
+
+    await this.newTransport()?.sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours Family!');
   }
 }
