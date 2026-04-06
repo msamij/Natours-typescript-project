201be528e0cc714f4edd78f259ca32d77bca70d2
Muhammad Sami <msamiaj@outlook.com>
2026-04-06 15:28:56 +0500

    feat: add /my-tours route

--- src/routes/viewRoutes.ts
+++ src/routes/viewRoutes.ts
@@ -9,6 +9,7 @@ router.get('/', createBookingCheckout, isLoggedIn, viewsController.getOverview);
 router.get('/tour/:slug', isLoggedIn, viewsController.getTour);
 router.get('/login', isLoggedIn, viewsController.getLoginForm);
 router.get('/me', protect, viewsController.getAccount);
+router.get('/my-tours', protect, viewsController.getMyTours);
 
 router.post('/submit-user-data', protect, viewsController.updateUserData);
 
